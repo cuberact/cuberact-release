e86b29560852e295de2c9dca8dd3d364cd68e0e2
NkD <michal.nikodim@topmonks.com>
2019-10-20 02:02:03 +0200

    release system improved, better variables, better flow

--- release-data/linux/install-desktop-entry.sh
+++ release-data/linux/install-desktop-entry.sh
@@ -1,16 +1,16 @@
-echo "Creating desktop entry for app #release.project#"
+echo "Creating desktop entry for app #{release.project}"
 
-file_location=~/.local/share/applications/#release.project#.desktop
+file_location=~/.local/share/applications/#{release.project}.desktop
 
 echo "[Desktop Entry]" > $file_location
 {
   echo "Version=1.0"
   echo "Type=Application"
-  echo "Name=#release.project.name#"
+  echo "Name=#{release.project.name}"
   echo "Icon=$(dirname $(readlink -f $0))/icon.png"
-  echo "Exec=$(dirname $(readlink -f $0))/#release.project#"
-  echo "Comment=#release.linux.comment#"
-  echo "Categories=#release.linux.category#"
+  echo "Exec=$(dirname $(readlink -f $0))/#{release.project}"
+  echo "Comment=#{release.var.linux.comment}"
+  echo "Categories=#{release.var.linux.category}"
   echo "Terminal=false"
 } >> $file_location
 
